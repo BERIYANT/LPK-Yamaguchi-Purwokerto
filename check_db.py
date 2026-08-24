945b9c7473dbc775b3bd10b537ec93a2e95220c5
BERIYANT <brianfe25@gmail.com>
2026-08-24 09:20:06 +0700

    Refactor code structure and optimize performance across multiple modules

--- check_db.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from utils.database import get_db
-
-with app.app_context():
-    db = get_db()
-    cur = db.cursor()
-    cur.execute("SHOW COLUMNS FROM users;")
-    columns = [row[0] for row in cur.fetchall()]
-    print("Columns:", columns)
--- test_pdf.py
+++
@@ -1,6 +0,0 @@
-from utils.pdf_generator import generate_registration_pdf
-try:
-    res = generate_registration_pdf({})
-    print(type(res), len(res))
-except Exception as e:
-    print("Error:", e)
