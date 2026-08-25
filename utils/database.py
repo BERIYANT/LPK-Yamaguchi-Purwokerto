13c59a32e86b490bca9be19c5d81c4b218f3a8e5
BERIYANT <brianfe25@gmail.com>
2026-08-25 19:59:41 +0700

    Reload private database settings for every connection

--- utils/database.py
+++ utils/database.py
@@ -1,10 +1,22 @@
 import mysql.connector
+import os
 from flask import g
-from config import Config
+from config import Config, load_private_environment
 
 def get_db():
     if 'db' not in g:
-        g.db = mysql.connector.connect(**Config.DB_CONFIG)
+        # Worker Passenger dapat mempertahankan modul Config lama. Muat ulang
+        # file privat tepat sebelum membuka koneksi agar request production
+        # tidak pernah kembali memakai fallback lokal root/root.
+        load_private_environment()
+        db_config = {
+            'host': os.getenv('DB_HOST', Config.DB_CONFIG['host']),
+            'user': os.getenv('DB_USER', Config.DB_CONFIG['user']),
+            'password': os.getenv('DB_PASSWORD', Config.DB_CONFIG['password']),
+            'database': os.getenv('DB_NAME', Config.DB_CONFIG['database']),
+            'port': int(os.getenv('DB_PORT', str(Config.DB_CONFIG['port']))),
+        }
+        g.db = mysql.connector.connect(**db_config)
     return g.db
 
 def close_db(error=None):
